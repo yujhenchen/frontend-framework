7024a57a568fbb957c4edd04b8b215b70732e764
Jen C. <65657219+yujhenchen@users.noreply.github.com>
2024-10-19 17:52:55 +0800

    fix: virtual node types, implement hFragment, add note

--- src/h.ts
+++ src/h.ts
@@ -6,7 +6,18 @@ export const DOM_TYPES = {
     FRAGMENT: 'fragment',
 } as const;
 
-export function h(tag: string, props: Record<string | number | symbol, unknown> = {}, children = {}) {
+type DomType = typeof DOM_TYPES[keyof typeof DOM_TYPES];
+
+type PropsType = Record<string | number | symbol, unknown>;
+
+export type ElementNodeType = {
+    tag: DomType,
+    props: PropsType,
+    children: Array<null | ElementNodeType| string>
+};
+
+// return a virtual node object
+export function h(tag: DomType, props: PropsType = {}, children: Array<null | ElementNodeType | string>) {
     return {
         tag,
         props,
@@ -15,10 +26,17 @@ export function h(tag: string, props: Record<string | number | symbol, unknown> 
     }
 }
 
-function mapTextNodes(children) {
+function mapTextNodes(children: Array<null | ElementNodeType | string>) {
     return children.map(child => typeof child === "string" ? hString(child) : child);
 }
 
 function hString(str: string) {
     return { type: DOM_TYPES.TEXT, value: str }
+}
+
+function hFragment(children: Array<null | ElementNodeType | string>) {
+    return {
+        type: DOM_TYPES.FRAGMENT,
+        children: mapTextNodes(withoutNulls(children))
+    }
 }
--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -1,4 +1,6 @@
-export function withoutNulls(arr: Array<Record<string | number | symbol, unknown>>) {
+import type { ElementNodeType } from "../h";
+
+export function withoutNulls(elementNodes: Array<null | ElementNodeType | string>) {
     // Note the use of the operator, as opposed to !==. You use this operator to remove != both and values null undefined
-    return arr.filter(item => item != null);
+    return elementNodes.filter(item => item != null);
 }
