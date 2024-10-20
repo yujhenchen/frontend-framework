4cf0afecf248abf66bab610a63ff382b23c2df47
Jen C. <65657219+yujhenchen@users.noreply.github.com>
2024-10-20 19:26:14 +0800

    feat: add setArrtibutes structure, implement setClass

--- src/utils/attributes.ts
+++ src/utils/attributes.ts
@@ -1,3 +1,54 @@
 export function setArrtibutes(el: Element, attrs) {
+    const { class: className, style, ...otherAttrs } = attrs;
+
+    if(className) {
+        setClass(el, className);
+    }
+
+    if(style) {
+        Object.entries(style).forEach(([prop, value]) => {
+            setStyle(el, prop, value);
+        });
+    }
+
+    for(const [name, value] of Object.entries(otherAttrs)) {
+        setArrtibute(el, name, value);
+    }
+}
+
+/**
+ * 
+ * @param el 
+ * @param className 
+ * 
+ * The DOMTokenList interface represents a set of space-separated tokens. 
+ * Such a set is returned by Element.classList or HTMLLinkElement.relList, and many others. 
+ */
+function setClass(el: Element, className: string | Array<string>) {
+    el.className = "";
+
+    if(typeof className === "string") {
+        el.className = className;
+    }
+
+    if(Array.isArray(className)) {
+        el.classList.add(...className);
+    }
+}
+
+/**
+ * 
+ * @param el 
+ * @param prop 
+ * @param value 
+ * 
+ * The CSSStyleDeclaration interface represents an object that is a CSS declaration block,
+ *  and exposes style information and various style-related methods and properties.
+ */
+function setStyle(el: Element, prop, value) {
+
+}
+
+function setArrtibute(el: Element, name, value) {
 
 }
