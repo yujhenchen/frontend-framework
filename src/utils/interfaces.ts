5de68a4d2de1e6816943e294edadec5081867543
yujhenchen <yujhen.chen93@gmail.com>
2024-10-27 20:23:27 +0800

    fix: make type optional git status

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,10 +1,15 @@
 import type { DOM_TYPES } from "../h";
 import type { VirtualNodeType } from "./types";
 
-// export type PropsType = Record<string | number | symbol, unknown>;
-export interface Props {
+
+export interface Attrs {
+    class: string;
+    style: Partial<CSSStyleDeclaration>;
+    [key: string]: string | Partial<CSSStyleDeclaration>;
+}
+
+export interface Props extends Attrs {
     on?: Record<string, (event?: Event | undefined) => void>
-    [key: string]: string | Record<string, (event?: Event | undefined) => void> | undefined; // Allow for additional properties
 };
 
 export interface ElementNode {
@@ -13,7 +18,7 @@ export interface ElementNode {
     children: Array<VirtualNodeType>,
     type: typeof DOM_TYPES.ELEMENT,
     el: Element | null,
-    listeners: Record<string, (event?: Event | undefined) => void>
+    listeners?: Record<string, (event?: Event | undefined) => void>
 }
 
 export interface FragmentNode {
@@ -26,10 +31,4 @@ export interface TextNode {
     type: typeof DOM_TYPES.TEXT,
     value: string,
     el: Text | null
-}
-
-export interface Attrs {
-    class: string;
-    style: Partial<CSSStyleDeclaration>;
-    [key: string]: string | Partial<CSSStyleDeclaration>;
 }
