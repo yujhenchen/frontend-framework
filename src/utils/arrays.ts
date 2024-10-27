ba04ae53e535329a7bbbea59f87f2ca20916037f
yujhenchen <yujhen.chen93@gmail.com>
2024-10-27 20:07:34 +0800

    fix: import path

--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -1,4 +1,4 @@
-import type { VirtualNodeType } from "../h";
+import type { VirtualNodeType } from "./types";
 
 export function withoutNulls(elementNodes: Array<VirtualNodeType | null | string>) {
     // Note the use of the operator, as opposed to !==. You use this operator to remove != both and values null undefined
