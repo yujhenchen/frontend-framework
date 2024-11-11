a2cfb404559d0fef35e64d86f031c9925a430d82
yujhenchen <yujhen.chen93@gmail.com>
2024-11-11 21:39:00 +0800

    feat: add afterEveryCommand, fix function definition args and return type

--- src/dispatcher.ts
+++ src/dispatcher.ts
@@ -1,7 +1,8 @@
 export class Dispatcher {
-    private subs = new Map<string, Array<() => void>>();
+    private subs = new Map<string, Array<(...args: unknown[]) => unknown>>();
+    private afterHandlers: ((...args: unknown[]) => unknown)[] = [];
 
-    public subscribe(commandName: string, handler: () => void) {
+    public subscribe(commandName: string, handler: (...args: unknown[]) => unknown) {
         if (!this.subs.has(commandName)) {
             this.subs.set(commandName, []);
         }
@@ -22,6 +23,15 @@ export class Dispatcher {
             // NOTE: when idx === -1, which removes the last handler in the handlers array (silent failure)
             const idx = handlers.indexOf(handler);
             handlers.splice(idx, 1);
+        }
+    }
+
+    public afterEveryCommand(handler: (...args: unknown[]) => unknown) {
+        this.afterHandlers.push(handler);
+
+        return () => {
+            const idx = this.afterHandlers.indexOf(handler);
+            this.afterHandlers.splice(idx, 1);
         }
     }
 }
